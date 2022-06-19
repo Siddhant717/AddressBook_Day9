0416e26c0b47002b115002081a07fcaf384f0324
Siddhant <santoshshrivastava39@gmail.com>
2022-06-19 22:23:02 +0530

    UC1 solution added

--- AddressBookProblems/Program.cs
+++ AddressBookProblems/Program.cs
@@ -8,6 +8,25 @@ namespace AddressBookProblems
         {
             Console.WriteLine("Welcome to AddressBook Program");
 
+            Console.WriteLine("Contact Details that you have provided- ");
+            Contact contact = new Contact();
+
+            contact.FirstName = "Siddhant";
+            contact.LastName = "Shrivastava";
+            contact.Address = "PQR";
+            contact.City = "Lucknow";
+            contact.State = "UP";
+            contact.Zipcode = 676567;
+            contact.PhoneNumber = 8888666666;
+            contact.Email = "Sid@gmail.com";
+            string statement = "FirstName " + contact.FirstName + " LastName " + contact.LastName + " Address " + contact.Address +
+                             " City " + contact.City + " State " + contact.State + " Zipcode " + contact.Zipcode + " PhoneNumber "
+                               + contact.PhoneNumber + " Email " + contact.Email;
+
+
+            Console.WriteLine(statement);
+
+
         }
     }
 
